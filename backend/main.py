8be23846d78ddea9d88f8cafa890f063595bc036
Rayiku Nashira <rayikume@gmail.com>
2024-07-24 04:22:57 +0300

    improved history algorithem

--- backend/main.py
+++ backend/main.py
@@ -15,9 +15,11 @@ for message in st.session_state.messages:
     if message["role"] == "assistant":
         with st.chat_message(message["role"], avatar="🤓"):
             st.markdown(message["content"])
+            textlist.append(message["content"])
     else:
         with st.chat_message(message["role"], avatar="🐤"):
             st.markdown(message["content"])
+            textlist.append(message["content"])
 
 promptlit = st.chat_input("Enter your prompt here")
 
@@ -25,17 +27,17 @@ if promptlit:
     with st.chat_message("user", avatar="🐤"):
         st.markdown(promptlit)
     st.session_state.messages.append({"role": "user", "content": promptlit})
-    textlist += promptlit
 
-    longtext = "".join(textlist)
-    template = "Remember this convo: {history} Answer this Question: {question}"
+    longtext = " ".join(textlist)
+    template = """You're Nerd AI, always begine your reply with 'well actuaklly'.
+    Make your tone sounds nerdy and thinking highly of yourself. 
+    Your job is to help the user by answering his/her questions. 
+    Answer this Question: {question}.
+    The conversation history between you and the user (DON'T MENTION CONVO HISTORY TO THE USER): {history}"""
     prompt = ChatPromptTemplate.from_template(template)
     chain = prompt | model
     response = chain.invoke({'history': {longtext}, 'question': f"{promptlit}"})
-    print(textlist)
-    print(st.session_state.messages)
 
     with st.chat_message("assistant", avatar="🤓"):
         st.markdown(response)
-        textlist += response
     st.session_state.messages.append({"role": "assistant", "content": response})
