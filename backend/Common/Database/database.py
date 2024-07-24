3f33752e152cb4a05da7906093a29f4552b5c47e
rayikume <rayikume@gmail.com>
2024-07-24 15:45:11 +0300

    added lemmatizer

--- backend/Common/Database/database.py
+++ backend/Common/Database/database.py
@@ -1,14 +1,44 @@
 import pandas as pd
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from langchain_text_splitters import CharacterTextSplitter
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+import chromadb
+import time
+# start = time.time()
 
-df = pd.read_csv('books.csv')
+chroma_client = chromadb.PersistentClient(path="/Users/aalbusayla001/Desktop/July2024 Projects/Occult.AI/Chroma_db")
+book_collection = chroma_client.get_or_create_collection(name="Book_Collection")
+# dataset = []
 
-books_df_cleaned = df.copy()
+# df = pd.read_csv('books.csv')
+# df = df.loc[6001:6809]
+# books_df_cleaned = df.copy()
+# print(df)
+# categorical = ['authors', 'subtitle', 'categories', 'thumbnail', 'description']
+# numerical = ['published_year', 'average_rating', 'num_pages', 'ratings_count']
 
-categorical = ['authors', 'subtitle', 'categories', 'thumbnail', 'description']
-numerical = ['published_year', 'average_rating', 'num_pages', 'ratings_count']
+# for column in categorical:
+#     books_df_cleaned.fillna({f'{column}': 'unknown'}, inplace=True)
 
-for column in categorical:
-    books_df_cleaned.fillna({f'{column}': 'unknown'}, inplace=True)
+# for column in numerical:
+#     books_df_cleaned.fillna({f'{column}': -1}, inplace=True)
 
-for column in numerical:
-    books_df_cleaned.fillna({f'{column}': -1}, inplace=True)
+# def getBooks(row):
+#     text = f"book title: {row['title']}, catagories: {row['categories']}, subtitle: {row['subtitle']}, description: {row['description']}"
+#     dataset.append(text)
+
+# books_df_cleaned.apply(getBooks, axis=1)
+
+# ids1 = books_df_cleaned["isbn10"].to_list()
+
+# book_collection.upsert(
+#     documents=dataset,
+#     ids=ids1
+# )
+
+# print(time.time() - start)
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
+import re
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+from Common.Database.database import book_collection
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
-model = OllamaLLM(model="llama3")
+model = OllamaLLM(model="mistral")
 textlist = []
 
 # Chatbox
@@ -23,20 +30,40 @@ for message in st.session_state.messages:
 
 promptlit = st.chat_input("Enter your prompt here")
 
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    return ' '.join(tokens)
+
 if promptlit:
     with st.chat_message("user", avatar="🐤"):
         st.markdown(promptlit)
+        filtered_text = preprocess_text(promptlit)
     st.session_state.messages.append({"role": "user", "content": promptlit})
 
+    db = book_collection.query(
+        query_texts=filtered_text,
+        n_results=5
+    )
     longtext = " ".join(textlist)
-    template = """You're Nerd AI, always begine your reply with 'well actuaklly'.
-    Make your tone sounds nerdy and thinking highly of yourself. 
-    Your job is to help the user by answering his/her questions. 
-    Answer this Question: {question}.
-    The conversation history between you and the user (DON'T MENTION CONVO HISTORY TO THE USER): {history}"""
+    template = """You're an AI assistance named Nerd AI that suggust books for users, 
+    you can also conversate with the user but always steer the conversation back to book recommendation,
+    the question of the user: {question}
+    Suggust only the books in this database: {database}
+    to remember the conversation with the user, here's all the interaction you've had with the user: {history}
+    """
     prompt = ChatPromptTemplate.from_template(template)
     chain = prompt | model
-    response = chain.invoke({'history': {longtext}, 'question': f"{promptlit}"})
+    response = chain.invoke({
+        'database': db.get("documents"),
+        'question': promptlit,
+        'history': longtext
+    })
 
     with st.chat_message("assistant", avatar="🤓"):
         st.markdown(response)
