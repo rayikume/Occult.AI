218e19f0ae9a3d0cc270fba4695fd28f6c163caf
Rayiku Nashira <rayikume@gmail.com>
2024-08-01 01:40:22 +0300

    customized Header

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -3,9 +3,20 @@ import HeaderCSS from "./Header.module.css";
 const Header = () => {
   return (
     <div id={HeaderCSS.flex}>
-      <div className={HeaderCSS.title}>Occult.AI</div>
+      <div className={HeaderCSS.title}>
+        <div className={HeaderCSS.logo}>Occult.AI</div>
+        <div className={HeaderCSS.userLogo}>USERLOGO</div>
+      </div>
       <div className={HeaderCSS.search_container}>
-        <input type="text" className={HeaderCSS.search_engine} />
+        <button className={HeaderCSS.search_icon}>SEARCHICON</button>
+        <input
+          type="text"
+          className={HeaderCSS.search_engine}
+          placeholder="Type book title/genre/name of author"
+        />
+        <button className={HeaderCSS.search_icon}>FILTER</button>
+        <div className={HeaderCSS.gap}></div>
+        <button className={HeaderCSS.like_filter}>LIKE</button>
       </div>
     </div>
   );
