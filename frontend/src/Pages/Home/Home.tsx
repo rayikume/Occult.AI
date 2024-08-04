fb214b2ebeb70f30630f2ed08c81a6d00cb89674
rayikume <rayikume@gmail.com>
2024-08-04 16:21:37 +0300

    added chatbox logo

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,14 +1,17 @@
 import Header from "../../Components/Header/Header";
 import BookShelf from "../../Components/BookShelf/BookShelf";
 import HomeCSS from "./Home.module.css";
+import ChatIcon from "../../Assets/ChatIcon.svg";
 
 const Home = () => {
   return (
-    <>
+    <div className={HomeCSS.homepage}>
       <Header />
       <BookShelf />
-      <div className={HomeCSS.footergap}></div>
-    </>
+      <div className={HomeCSS.chatlogo_container}>
+        <img src={ChatIcon} className={HomeCSS.chatbot} />
+      </div>
+    </div>
   );
 };
 
